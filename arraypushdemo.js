eddc74613026457206446efc95eb577f181a1e27
H2oFall <42394085+H2oFall@users.noreply.github.com>
2018-08-16 13:42:44 -1000

    first commit
    this is the first commit

---
+++ arraypushdemo.js
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <script>
+//create empty array
+var firstNames = [];
+var lastNames = [];
+var first = prompt("what is your first name?")
+//ask user for name
+var last = prompt("what is your last name name?")
+firstNames.push(first);
+lastNames.push(last);
+//display everything to screeen
+window.alert(firstNames + lastNames + firstNames.length + lastNames.length);
+
+    </script>
+  </head>
+  <body>
+
+  </body>
+</html>
